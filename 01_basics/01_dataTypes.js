e7150a1ecf8889c48d6c4243f6d588a31ff64029
Raj Kumar <34243150+RajKumarPal@users.noreply.github.com>
2025-04-28 13:07:05 +0000

    primitive

--- 01_basics/01_dataTypes.js
+++ 01_basics/01_dataTypes.js
@@ -1,2 +1,10 @@
 //DataTypes
 //1-Primitive Data Types
+// Primitive data types stored by value;
+// 1 - String
+// 2 - Number
+// 3 - Bollean
+// 4 - Bigint
+// 5 - Symbol
+// 6 - Null;
+// 7 - undefined;
